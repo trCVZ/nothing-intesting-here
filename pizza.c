2426dcdd6e435285bde7656b81b53da67347f5e3
trCVZ <tristan.cvz.pro@gmail.com>
2025-03-08 14:43:28 +0100

    Initialize pizza structure

---
+++ pizza.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+#define MAX_INGREDIENTS 10
+#define MAX_NAME_LENGTH 20
+
+typedef struct ingredient {
+    char name[MAX_NAME_LENGTH];
+    float price;
+} ingredient;
+
+typedef struct pizza {
+    ingredient ingredients[MAX_INGREDIENTS];
+    float price;
+    char name[MAX_NAME_LENGTH];
+} pizza;
+
+const ingredient ingredient_list[] = {
+    {"Tomato Sauce", 0.5},
+    {"Chicken", 2.00},
+    {"Arugula", 1.00},
+    {"Goat Cheese", 1.50},
+    {"Beef", 2.50}
+};
+
+ingredient find_ingredient_by_name(const char* name) {
+    for (size_t i = 0; i < sizeof(ingredient_list) / sizeof(ingredient_list[0]); i++) {
+        if (strcmp(ingredient_list[i].name, name) == 0) {
+            return ingredient_list[i];
+        }
+    }
+    ingredient not_found = { "", 0.0 };
+    return not_found;
+}
+
+void init_pizza(pizza* pizza, const char* name, const char* ingredient_names[], size_t num_ingredients) {
+    if (num_ingredients > MAX_INGREDIENTS) {
+        num_ingredients = MAX_INGREDIENTS;
+    }
+
+    strncpy(pizza->name, name, MAX_NAME_LENGTH - 1);
+    pizza->name[MAX_NAME_LENGTH - 1] = '\0';
+
+    pizza->price = 0.0;
+
+    for (size_t i = 0; i < num_ingredients; i++) {
+        ingredient ing = find_ingredient_by_name(ingredient_names[i]);
+        if (ing.price != 0.0) {
+            pizza->ingredients[i] = ing;
+            pizza->price += ing.price;
+        }
+    }
+
+    pizza->price = roundf(pizza->price * 100) / 100;
+}
+
+void print_pizza(const pizza* p) {
+    printf("Pizza: %s\n", p->name);
+    printf("Ingredients:\n");
+    for (size_t i = 0; i < MAX_INGREDIENTS && p->ingredients[i].price != 0; i++) {
+        printf("  - %s: $%.2f\n", p->ingredients[i].name, p->ingredients[i].price);
+    }
+    printf("Total price: $%.2f\n", p->price);
+}
+
+int main() {
+    pizza pizza_deluxe;
+    const char* selected_ingredients[] = {
+        "Tomato Sauce",
+        "Chicken",
+        "Goat Cheese"
+    };
+    size_t num_selected_ingredients = sizeof(selected_ingredients) / sizeof(selected_ingredients[0]);
+
+    init_pizza(&pizza_deluxe, "Deluxe", selected_ingredients, num_selected_ingredients);
+    print_pizza(&pizza_deluxe);
+    return 0;
+}
